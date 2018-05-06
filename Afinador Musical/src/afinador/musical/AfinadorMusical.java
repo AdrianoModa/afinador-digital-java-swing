c1666f8275964a26e8ace8a9883de6097f748730
adrianomoda <adrianomodafeitosa@gmail.com>
2018-05-06 13:28:41 -0300

    Uso da classe Runtime para abrir Volume

--- Afinador Musical/src/afinador/musical/AfinadorMusical.java
+++ Afinador Musical/src/afinador/musical/AfinadorMusical.java
@@ -5,11 +5,11 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -110,9 +110,11 @@ public class AfinadorMusical extends JFrame {
 		abrirVolume.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					java.awt.Desktop.getDesktop().open(new File("C:\\Windows\\System32\\SndVol.exe"));
-				} catch (IOException errodearquivo) {					
-					errodearquivo.printStackTrace();
+					Runtime.getRuntime().exec("sndvol");
+				} catch (RuntimeException erroRuntime) {
+					erroRuntime.printStackTrace();
+				} catch (IOException erroDeArquivo) {					
+					erroDeArquivo.printStackTrace();
 					JOptionPane.showMessageDialog(null,"Arquivo inexistente ou corrompido!", "Erro de Arquivo",JOptionPane.ERROR_MESSAGE);
 				}
 			}
